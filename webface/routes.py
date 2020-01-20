c835961bbc7b70aedb44d4352a10a0901f6d774c
Marrek Nožka <nozka@spseol.cz>
2020-01-20 08:55:25 +0100

    kontrola uzivatele

--- webface/routes.py
+++ webface/routes.py
@@ -1,6 +1,20 @@
 from . import app
-from flask import render_template
+from .models import Uzivatel
+from flask import render_template, request
+from pony.orm import db_session
+from werkzeug.security import check_password_hash
+
 
 @app.route('/')
 def index():
+    login = request.form.get('login')
+    heslo = request.form.get('heslo')
+    print(login)
+    with db_session:
+        uzivatel = Uzivatel.get(login=login)
+        if uzivatel:
+            pwhash = uzivatel.heslo
+        else:
+            pwhash = None
+    check_password_hash(pwhash, heslo)
     return render_template('base.html.j2')
