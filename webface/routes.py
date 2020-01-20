3d7fd426187ca176c0e67d887e3551065c87e189
Marrek Nožka <nozka@spseol.cz>
2020-01-20 10:16:20 +0100

    GET -> POST

--- webface/routes.py
+++ webface/routes.py
@@ -1,20 +1,26 @@
 from . import app
 from .models import Uzivatel
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from pony.orm import db_session
 from werkzeug.security import check_password_hash
 
 
-@app.route('/')
+@app.route("/", methods=["GET"])
 def index():
-    login = request.form.get('login')
-    heslo = request.form.get('heslo')
+    return render_template("base.html.j2")
+
+
+@app.route("/", methods=["POST"])
+def index_post():
+    login = request.form.get("login")
+    heslo = request.form.get("heslo")
     print(login)
-    with db_session:
-        uzivatel = Uzivatel.get(login=login)
-        if uzivatel:
-            pwhash = uzivatel.heslo
-        else:
-            pwhash = None
-    check_password_hash(pwhash, heslo)
-    return render_template('base.html.j2')
+    if login and heslo:
+        with db_session:
+            uzivatel = Uzivatel.get(login=login)
+            if uzivatel:
+                pwhash = uzivatel.heslo
+            else:
+                pwhash = None
+        check_password_hash(pwhash, heslo)
+    return redirect(url_for("index"))
